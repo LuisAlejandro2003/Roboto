aac1966e116e250d0f5d7f57283c9b5446cd769c
LuisAlejandro2003 <luisalejandro2003121822@gmail.com>
2023-03-01 19:33:52 -0600

    modificando la rama dev

---
+++ src/components/molecules/OpcionesNavAdmin.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Label from "../atoms/Label";
+
+ 
+const OpcionesNavAdmin = ({ msn, estilo , pequenio }) => {
+    return ( 
+       <Label msn={msn} estilo={estilo} pequenio={pequenio}></Label>
+      
+     );
+}
+ 
+export default OpcionesNavAdmin;
